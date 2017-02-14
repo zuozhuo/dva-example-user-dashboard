bf99637c27aacdd1709e00ca7a430abd935d87f1
zuozhuo <zuozhuo@xmzhen.com>
2017-02-14 11:20:44 +0800

    fix

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,45 @@
+/**
+ * Created by zuozhuo on 2017/2/5.
+ */
 "use strict";
 import fetch from 'dva/fetch';
 import querystring from 'querystring';
 import $$ from "./appHelper";
+
+function requestGlobalConfig(url, options = {}) {
+  // 这里可以做一些全局的ajax请求配置
+  url = $$.addUrlQuery(url, {
+    lhotelGroupCode: window.lhotelGroupCode,
+    ticket: sessionStorage.ticket,
+    ark_version: '1.0.0',
+  });
+
+  // 全局ajax添加头
+  options.headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'X-Request-Client-Name': 'ark-admin-api-request',
+    ...options.headers,
+  };
+
+  return {
+    url,
+    options,
+  }
+}
+
+function createBodyString(data) {
+  // 以JSON形式提交
+  const bodyString = typeof data === 'string' ? data : JSON.stringify(data);
+  // 以formData（querystring）形式提交
+  // const bodyString = querystring.stringify(data);
+
+  return bodyString;
+}
+
+function parseJSON(response) {
+  return response.json();
+}
 
 function checkStatus(response) {
   if (response.status >= 200 && response.status < 300) {
@@ -20,27 +58,16 @@ function checkStatus(response) {
  * @param  {object} [options] The options we want to pass to "fetch"
  * @return {object}           An object containing either "data" or "err"
  */
-export async function request(url, options) {
-  // TODO 这里可以做一些全局处理，追加url请求参数，追加header等
-  const response = await fetch(url, options);
-
-  checkStatus(response);
-
-  const data = await response.json();
-
-  const ret = {
-    data,
-    headers: {},
-  };
-
-  if (response.headers.get('x-total-count')) {
-    ret.headers['x-total-count'] = response.headers.get('x-total-count');
-  }
-
-  return ret;
+function request(url, options) {
+  const globalConfig = requestGlobalConfig(url, options);
+  return fetch(globalConfig.url, globalConfig.options)
+    .then(checkStatus)
+    .then(parseJSON)
+    .then((data) => ({data}))
+    .catch((err) => ({err}));
 }
 
-export async function GET(url, query = {}, options) {
+async function GET(url, query = {}, options) {
   const urlWithQuery = $$.addUrlQuery(url, query);
 
   return await request(urlWithQuery, {
@@ -49,16 +76,7 @@ export async function GET(url, query = {}, options) {
   });
 }
 
-export async function POST(url, data = {}, options) {
-
-  return await  request(url, {
-    method: 'POST',
-    body: createBodyString(data),
-    ...options,
-  })
-}
-
-export async function DELETE(url, query = {}, options) {
+async function DELETE(url, query = {}, options) {
   const urlWithQuery = $$.addUrlQuery(url, query);
 
   return await  request(urlWithQuery, {
@@ -67,7 +85,16 @@ export async function DELETE(url, query = {}, options) {
   });
 }
 
-export async function PATCH(url, data = {}, options) {
+async function POST(url, data = {}, options) {
+
+  return await  request(url, {
+    method: 'POST',
+    body: createBodyString(data),
+    ...options,
+  })
+}
+
+async function PATCH(url, data = {}, options) {
 
   return await request(url, {
     method: 'PATCH',
@@ -76,12 +103,20 @@ export async function PATCH(url, data = {}, options) {
   })
 }
 
+async function PUT(url, data = {}, options) {
 
-function createBodyString(data) {
-  // 以JSON形式提交
-  const bodyString = typeof data === 'string' ? data : JSON.stringify(data);
-  // 以formData（querystring）形式提交
-  // const bodyString = querystring.stringify(data);
+  return await request(url, {
+    method: 'PUT',
+    body: createBodyString(data),
+    ...options,
+  })
+}
 
-  return bodyString;
+export {
+  request,
+  GET,
+  POST,
+  DELETE,
+  PATCH,
+  PUT,
 }
